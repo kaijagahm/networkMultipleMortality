def63a818f93a0570d041a120b9a7f3eb3662d9f
Kaija <kaija.gahm@gmail.com>
2022-04-19 12:20:38 -0700

    list pkgs to avoid loading in each script

---
+++ libraries.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(move) # for downloading data
+library(moveVis)
+library(mapproj) # necessary for working with Google maps
+library(ggmap) # necessary for working with Google maps
+library(spatsoc) # for working with the SN parts
+library(asnipe) # for working with the SN parts
+library(igraph) # for working with the SN parts
+library(reshape) # for the manual selection where i build the SN myself
+library(data.table) # for the manual section where i build the SN myself
+library(adehabitatLT)
+library(rgdal)
+library(rgeos)
+library(sf)
+library(plyr) # for duration filtering
+library(lubridate) # for dealing with dates
+library(here) # for file paths
+library(igraph)
